192c05e34b55f48db8d2f18b6f11c96ee4e505b6
lebrancconvas <lebrancconvas@gmail.com>
2022-04-20 16:15:53 +0700

    ✨ Add Heading.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
+import Heading from './components/Heading';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <Heading />
     </div>
   );
 }
---
+++ web/src/components/Heading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Heading = () =>
+{
+	return(
+		<header>
+			<h1>Poom Yimyuean</h1>
+		</header>
+	)
+}
+
+export default Heading;
